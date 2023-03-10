fa4687a85d4215ab3722cd73e19e4c8e8b7089fb
ikarmus2001 <kacpiczek@gmail.com>
2023-03-10 15:48:59 +0100

    Added ZadMainExample.cc, lectures 1

---
+++ kcppZadania/ZadMainExample.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+char* pointerToLetterA()
+{
+    char* letterA = 'a';
+    return letterA;
+}
+
+// int
+
+// float 
+
+// extern "C"
+
+// extern
+
+int main()
+{
+    
+}
